a910616f7647c864bd6e088cb5f47f143b454080
duocvu <21A100100100@students.hou.edu.vn>
2024-10-09 15:32:18 +0700

    Learn text field in jetpack

--- app/src/main/java/com/example/learnaboutjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnaboutjetpackcompose/MainActivity.kt
@@ -1,49 +1,37 @@
 package com.example.learnaboutjetpackcompose
 
-import android.icu.text.ListFormatter.Width
+import android.R.style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.learnaboutjetpackcompose.ui.theme.LearnAboutJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -60,40 +48,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen(){
-    Column (modifier = Modifier.padding(24.dp)) {
-        BannerCompose()
-        Spacer(Modifier.padding(10.dp))
-        CircleAvatar()
+    Column (modifier = Modifier.padding(top = 30.dp)) {
+        DemoTextField()
     }
 
 }
-val customH1 :TextStyle
-    get() = TextStyle(
-        color = Color.Magenta,
-
-    )
 
 @Composable
-fun Greeting() {
-    Text(
-        text = stringResource(id = R.string.test_text),
-        maxLines = 1,
-        style = customH1
+fun DemoTextField (){
+    var firstName by remember {
+        mutableStateOf("")
+    }
+    TextField(value = firstName, onValueChange = {
+        firstName = it
+    },
+        textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+        label = { Text("First Name") },
+        placeholder = {Text("Enter your name")},
+        leadingIcon = { Icon(Icons.Default.Person,"") },
+        trailingIcon = {
+            IconButton(onClick = {
+                firstName = ""
+            }) {
+                Icon(Icons.Default.Close,"")
+            }
+        },
+        colors = TextFieldDefaults.colors(
+            cursorColor = Color.Black,
+            unfocusedTextColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.White,
+            errorIndicatorColor = Color.Transparent
+        ),
+        shape = RoundedCornerShape(12.dp),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Phone
+        )
     )
-}
-
-@Composable
-fun MultipleStyle(){
-    Text( text = buildAnnotatedString {
-        withStyle( style = SpanStyle(color = Color.Blue)){
-            append("H")
-        }
-        append("ello")
-        withStyle( style = SpanStyle(color = Color.Red)){
-            append("D")
-        }
-        append("uoc!")
-    })
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -104,43 +96,5 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun BannerCompose() {
-    Image(
-        painter = painterResource(id = R.drawable.banner),
-        contentDescription = null,
-        modifier = Modifier
-            .width(350.dp) // Cố định chiều rộng
-            .wrapContentHeight(), // Tự động điều chỉnh chiều cao
-        contentScale = ContentScale.FillWidth // Kéo dài ảnh để lấp đầy chiều rộng
-    )
-}
 
 
-@Composable
-fun CircleAvatar() {
-    Surface(
-        modifier = Modifier.border(
-            BorderStroke(
-                2.dp,
-                color = Color.Gray
-            ), shape = CircleShape).clip(shape = CircleShape)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.avatar),
-            contentDescription = "circle avatar",
-            contentScale = ContentScale.Inside
-            )
-    }
-
-}
-
-@Composable
-fun vectorImageComponent(){
-    Image(imageVector = Icons.Filled.Person, contentDescription = "Person" )
-}
-
-@Composable
-fun CustomPainterImageCompose(){
-    Image( ColorPainter(Color.Red) , contentDescription = null)
-}
