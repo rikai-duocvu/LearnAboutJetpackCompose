527b64a4245cc1814774db74ba8d12cdbf5a0618
duocvu <21A100100100@students.hou.edu.vn>
2024-10-10 10:27:11 +0700

    Learn Box, Row, Column

--- app/src/main/java/com/example/learnaboutjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnaboutjetpackcompose/MainActivity.kt
@@ -1,47 +1,35 @@
 package com.example.learnaboutjetpackcompose
 
-import android.icu.text.ListFormatter.Width
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learnaboutjetpackcompose.ui.theme.LearnAboutJetpackComposeTheme
@@ -49,10 +37,15 @@ import com.example.learnaboutjetpackcompose.ui.theme.LearnAboutJetpackComposeThe
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             LearnAboutJetpackComposeTheme {
-                HomeScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Gray
+                ) {
+                    HomeScreen()
+                }
             }
         }
     }
@@ -60,40 +53,89 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen(){
-    Column (modifier = Modifier.padding(24.dp)) {
-        BannerCompose()
-        Spacer(Modifier.padding(10.dp))
-        CircleAvatar()
+    //Box trong jetpack
+//    Box(
+//        modifier = Modifier.fillMaxSize() // Đảm bảo Box chiếm toàn bộ màn hình
+//    ) {
+//        BoxItem(Color.Blue, 200.dp, modifier = Modifier.align(Alignment.TopStart))
+//        BoxItem(Color.Red,150.dp, modifier = Modifier.align(Alignment.TopEnd))
+//        BoxItem(Color.Yellow, modifier =  Modifier.align(Alignment.BottomStart))
+//        BoxItem(Color.Magenta, modifier = Modifier.align(Alignment.BottomEnd))
+//        BoxItem(Color.Black, modifier = Modifier.align(Alignment.Center))
+//
+//    }
+    // Row trong jetpack
+//    Box(){
+////        Row (
+////            modifier = Modifier
+////                .background(Color.Gray)
+////                .size(400.dp, 300.dp),
+////            verticalAlignment = Alignment.CenterVertically,
+////            horizontalArrangement = Arrangement.SpaceBetween
+////        ){
+////            BoxItem(Color.Blue)
+////            BoxItem(Color.Red)
+////            BoxItem(Color.Green)
+////
+////        }
+////    }
+
+//    Box(){
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            OutlinedTextField(value = "" , onValueChange = {} , modifier = Modifier.weight( 2f))
+//            Icon(Icons.Default.Send, contentDescription = "Send", Modifier.size(36.dp).weight(1f))
+//        }
+//    }
+
+    //Column trong jetpack
+//    Box() {
+//        Column(
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            modifier = Modifier.size(500.dp, 700.dp).background(Color.Gray),
+//            verticalArrangement = Arrangement.SpaceBetween
+//        ) {
+//            BoxItem(Color.Blue)
+//            BoxItem(Color.Red)
+//            BoxItem(Color.Green)
+//        }
+//    }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Code has been send to 09123aaa231")
+                Spacer(Modifier.padding(24.dp))
+                OutlinedTextField(value = "" , onValueChange = {})
+                Spacer(Modifier.padding(24.dp))
+                Text("Resend in 53s")
+            }
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+                Text("Verify")
+            }
+        }
+
     }
-
-}
-val customH1 :TextStyle
-    get() = TextStyle(
-        color = Color.Magenta,
-
-    )
-
-@Composable
-fun Greeting() {
-    Text(
-        text = stringResource(id = R.string.test_text),
-        maxLines = 1,
-        style = customH1
-    )
 }
 
 @Composable
-fun MultipleStyle(){
-    Text( text = buildAnnotatedString {
-        withStyle( style = SpanStyle(color = Color.Blue)){
-            append("H")
-        }
-        append("ello")
-        withStyle( style = SpanStyle(color = Color.Red)){
-            append("D")
-        }
-        append("uoc!")
-    })
+fun BoxItem(color: Color, size: Dp = 100.dp, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .background(color = color)
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -104,43 +146,3 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun BannerCompose() {
-    Image(
-        painter = painterResource(id = R.drawable.banner),
-        contentDescription = null,
-        modifier = Modifier
-            .width(350.dp) // Cố định chiều rộng
-            .wrapContentHeight(), // Tự động điều chỉnh chiều cao
-        contentScale = ContentScale.FillWidth // Kéo dài ảnh để lấp đầy chiều rộng
-    )
-}
-
-
-@Composable
-fun CircleAvatar() {
-    Surface(
-        modifier = Modifier.border(
-            BorderStroke(
-                2.dp,
-                color = Color.Gray
-            ), shape = CircleShape).clip(shape = CircleShape)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.avatar),
-            contentDescription = "circle avatar",
-            contentScale = ContentScale.Inside
-            )
-    }
-
-}
-
-@Composable
-fun vectorImageComponent(){
-    Image(imageVector = Icons.Filled.Person, contentDescription = "Person" )
-}
-
-@Composable
-fun CustomPainterImageCompose(){
-    Image( ColorPainter(Color.Red) , contentDescription = null)
-}
