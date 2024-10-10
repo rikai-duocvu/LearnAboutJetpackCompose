0c8b0f87d2d139e68d630a552e460fa1c07220a7
duocvu <21A100100100@students.hou.edu.vn>
2024-10-10 17:36:11 +0700

    Learn ConstrantLayout

--- app/src/main/java/com/example/learnaboutjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnaboutjetpackcompose/MainActivity.kt
@@ -1,49 +1,32 @@
 package com.example.learnaboutjetpackcompose
 
-import android.icu.text.ListFormatter.Width
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.example.learnaboutjetpackcompose.ui.theme.LearnAboutJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,42 +44,109 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HomeScreen(){
     Column (modifier = Modifier.padding(24.dp)) {
-        BannerCompose()
-        Spacer(Modifier.padding(10.dp))
-        CircleAvatar()
+
     }
 
 }
-val customH1 :TextStyle
-    get() = TextStyle(
-        color = Color.Magenta,
-
-    )
 
 @Composable
-fun Greeting() {
-    Text(
-        text = stringResource(id = R.string.test_text),
-        maxLines = 1,
-        style = customH1
-    )
+fun Ingredient(
+    @DrawableRes icon: Int,
+    value:Int,
+    unit:String?,
+    name:String,
+    modifier: Modifier = Modifier
+    ){
+    val backgroundColor = Color(0xFFFEF9E4)
+    val borderColor = Color(0xFFFBE897).copy(0.7f)
+
+
+    ConstraintLayout(
+        modifier = modifier
+            .size(width = 104.dp, height = 104.dp)
+            .background(color = backgroundColor, shape = CircleShape)
+            .border(BorderStroke(width = 1.dp, color = borderColor))
+    ) {
+        val horizontalGuideLines = createGuidelineFromTop(0.6f)
+        val horizontalGuideLinesBottom = createGuidelineFromTop(0.5f)
+        val imgIcon = createRef()
+        Image(painter = painterResource(id = icon), contentDescription = null , modifier = Modifier.constrainAs(imgIcon){
+            top.linkTo(parent.top, margin = 3.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(horizontalGuideLines)
+            height= Dimension.fillToConstraints
+        })
+        val (tvValue,tvUnit,tvName) = createRefs()
+
+        val verticalGuideLineslf = createGuidelineFromStart(0.4f)
+        val verticalGuideLines = createGuidelineFromStart(0.5f)
+
+        val valueTextColor = Color(0xFFFB7D8A)
+        Text(text = value.toString() ,style= TextStyle(
+            color = valueTextColor,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 32.sp,
+            lineHeight = 14.sp
+        ),
+            modifier = Modifier.constrainAs(tvValue){
+                top.linkTo(horizontalGuideLinesBottom)
+                start.linkTo(parent.start,22.dp)
+                end.linkTo(verticalGuideLineslf)
+//                bottom.linkTo(parent.bottom)
+            }
+        )
+
+        unit?.let{
+            Text(text = unit, style = TextStyle(
+                color = valueTextColor,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 12.sp,
+                lineHeight = 14.sp
+            ),
+                modifier =Modifier.constrainAs(tvUnit){
+                    top.linkTo(tvValue.bottom)
+                    start.linkTo(parent.start,30.dp)
+                    end.linkTo(verticalGuideLines)
+                    bottom.linkTo(parent.bottom,12.dp)
+                })
+        }
+        val bottomBarrier = createBottomBarrier(tvValue,tvUnit)
+        val endGuideLine = createGuidelineFromEnd(0.1f)
+        Text(text = name, style = TextStyle(
+            color = Color(0xFF1E2742),
+            lineHeight = 14.sp,
+            fontSize = 12.sp,
+        ),
+            modifier = Modifier.constrainAs(tvName){
+                top.linkTo(tvValue.top)
+                start.linkTo(verticalGuideLines)
+                bottom.linkTo(bottomBarrier)
+                end.linkTo(endGuideLine)
+                width = Dimension.fillToConstraints
+            },
+            maxLines = 2,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun MultipleStyle(){
-    Text( text = buildAnnotatedString {
-        withStyle( style = SpanStyle(color = Color.Blue)){
-            append("H")
-        }
-        append("ello")
-        withStyle( style = SpanStyle(color = Color.Red)){
-            append("D")
-        }
-        append("uoc!")
-    })
+fun IngredientPreview(){
+    Row (){
+        Ingredient(
+            icon=R.drawable.ic_different2,
+            name= "Lemon Juice",
+            unit= "ml",
+            value = 30,
+            modifier = Modifier.size(100.dp))
+    }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+
+
+//@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LearnAboutJetpackComposeTheme {
@@ -104,43 +154,3 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun BannerCompose() {
-    Image(
-        painter = painterResource(id = R.drawable.banner),
-        contentDescription = null,
-        modifier = Modifier
-            .width(350.dp) // Cố định chiều rộng
-            .wrapContentHeight(), // Tự động điều chỉnh chiều cao
-        contentScale = ContentScale.FillWidth // Kéo dài ảnh để lấp đầy chiều rộng
-    )
-}
-
-
-@Composable
-fun CircleAvatar() {
-    Surface(
-        modifier = Modifier.border(
-            BorderStroke(
-                2.dp,
-                color = Color.Gray
-            ), shape = CircleShape).clip(shape = CircleShape)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.avatar),
-            contentDescription = "circle avatar",
-            contentScale = ContentScale.Inside
-            )
-    }
-
-}
-
-@Composable
-fun vectorImageComponent(){
-    Image(imageVector = Icons.Filled.Person, contentDescription = "Person" )
-}
-
-@Composable
-fun CustomPainterImageCompose(){
-    Image( ColorPainter(Color.Red) , contentDescription = null)
-}
